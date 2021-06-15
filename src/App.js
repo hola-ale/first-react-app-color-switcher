446b82348cef5d78f96e2b0a3b56c38423555baa
Ola Hola <homoncik.aleksandra@gmail.com>
2021-06-15 16:50:55 +0200

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
+import React, {useState} from "react"
 import './App.css';
+import ColorChangeButton from './ColorChangeButton'
 
 function App() {
+  const [color, setColor] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"react-root " + color }>
+      <div className="centered">
+        <h1>Pick a color!</h1>
+        <ColorChangeButton color="BLUE" setColor={setColor} />
+        <ColorChangeButton color="RED" setColor={setColor} />
+        <ColorChangeButton color="ORANGE" setColor={setColor} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
